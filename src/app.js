4e6b75b22574581d4f45e4fad69af6f11ff6d9f6
TarikCampestrini <tarikrc@hotmail.com>
2025-09-18 12:38:10 -0300

    configuração package json e ajustes no app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 import express from "express";
-import moradorRoutes from "./routes/moradorRoutes.js";
+import cors from "cors";
+import moradorRoutes from "../src/routes/moradorRoutes.js"; // ajuste caminho se necessário
 
 const app = express();
 
+// CORS
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.use(express.json());
 
-// Aqui é o ponto principal:
+// Rotas
 app.use("/api/moradores", moradorRoutes);
 
 export default app;
-

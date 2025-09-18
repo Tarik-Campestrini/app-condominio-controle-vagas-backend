3c741cc0d1f1aa6e91d0833b5f54001c984d7f54
TarikCampestrini <tarikrc@hotmail.com>
2025-09-18 14:49:27 -0300

    Ajustes na API para funcionar com o MongoDB Atlas

--- api/moradores.js
+++ api/moradores.js
@@ -1,24 +1,34 @@
 import connectDB from "../src/config/db.js";
-import Morador from "../src/models/Morador.js";
+import * as moradorController from "../src/controllers/moradorController.js";
 
-connectDB(); // Conecta ao MongoDB
+connectDB();
 
 export default async function handler(req, res) {
-  if (req.method === "GET") {
-    try {
-      const moradores = await Morador.find();
-      res.status(200).json(moradores);
-    } catch (err) {
-      res.status(500).json({ error: "Erro ao buscar moradores" });
+  const { method, query, body } = req;
+
+  try {
+    switch (method) {
+      case "GET":
+        const moradores = await moradorController.getMoradores();
+        return res.status(200).json(moradores);
+
+      case "POST":
+        const novo = await moradorController.createMorador(body);
+        return res.status(201).json(novo);
+
+      case "PUT":
+        const atualizado = await moradorController.updateMorador(query.id, body);
+        return res.status(200).json(atualizado);
+
+      case "DELETE":
+        await moradorController.deleteMorador(query.id);
+        return res.status(204).end();
+
+      default:
+        return res.status(405).json({ error: "Método não permitido" });
     }
-  } else if (req.method === "POST") {
-    try {
-      const novoMorador = await Morador.create(req.body);
-      res.status(201).json(novoMorador);
-    } catch (err) {
-      res.status(500).json({ error: "Erro ao criar morador" });
-    }
-  } else {
-    res.status(405).json({ error: "Método não permitido" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Erro no servidor" });
   }
 }

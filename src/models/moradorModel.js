550a80819291aef277efe2f9287418e6e7e65bb5
TarikCampestrini <tarikrc@hotmail.com>
2025-09-19 11:46:14 -0300

    Ajustes MongoDB connection and collection name

--- src/models/moradorModel.js
+++ src/models/moradorModel.js
@@ -4,6 +4,9 @@ const moradorSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   apto: { type: Number, required: true },
   telefone: { type: String, required: true },
-}, { timestamps: true });
+}, { 
+  timestamps: true,
+  collection: "moradores" 
+});
 
 export default mongoose.model("Morador", moradorSchema);

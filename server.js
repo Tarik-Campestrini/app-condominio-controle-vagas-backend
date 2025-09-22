6ab9065595cc31cf6b4e8d0bec5ac526d5b24827
TarikCampestrini <tarikrc@hotmail.com>
2025-09-22 15:04:01 -0300

    reestruturação  do Backend

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
-import app from "./src/services/app.js";
 import connectDB from "./src/config/db.js";
+import moradorRoutes from "./src/routes/moradorRoutes.js";
+const app = express();
 
-dotenv.config();
+app.use(cors());
+app.use(express.json());
 
-// Conectar ao banco
 connectDB();
+
+
+app.use("/api/moradores", moradorRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+  console.log("Conectado na Port", PORT);
+  
 });

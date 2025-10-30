8dd43d440a78bb862b11b3f8d8b87d2aa2d5c510
Tarik Ribeiro Constancio Campestrini <tarikrc@hotmail.com>
2025-10-30 12:43:15 -0300

    Ajuste na rota de vagas

--- src/routes/vagaRoutes.js
+++ src/routes/vagaRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createVaga, listarVagas, ocuparVaga, liberarVaga } from "../controllers/vagaController.js";
+import { createVaga, listarVagas, ocuparVaga, liberarVaga, deletarVaga } from "../controllers/vagaController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
